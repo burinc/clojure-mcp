756601d8f151c832da05a3c7f521e90b34c6b5b3
burinc <burin@duck.com>
2025-09-08 17:11:52 +1000

    Update start script (wip)
    - To reflect state of the codes

--- 02-start-shadow-cljs.sh
+++ 02-start-shadow-cljs.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -eo pipefail
-echo "About to start the shadow-cljs watch server..."
-cd /Users/choomnuanb/dev/github--chr15m--sitefox/examples/shadow-cljs-extended
-npx shadow-cljs watch server client
+#echo "About to start the shadow-cljs watch server..."
+#cd /Users/choomnuanb/dev/github--chr15m--sitefox/examples/shadow-cljs-extended
+
+#echo "About to start the b12n-newsapi watch app..."
+#cd /Users/choomnuanb/dev/b12n-newsapi
+#npx shadow-cljs watch app
+
+echo "About to start the b12n-cljsapp watch app..."
+cd /Users/choomnuanb/dev/b12n-cljsapp
+npx shadow-cljs watch app
--- 03-start-shadow-context.sh
+++ 03-start-shadow-context.sh
@@ -1,6 +1,22 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
 cd /Users/choomnuanb/dev/clojure-mcp
+
+#clojure -X:mcp-shadow-dual \
+#  :port 7888 \
+#  :shadow-port 7889 \
+#  :shadow-build '"server"' \
+#  :project-dir '"/Users/choomnuanb/dev/github--chr15m--sitefox/examples/shadow-cljs-extended"'
+
+# clojure -X:mcp-shadow-dual \
+#   :port 7888 \
+#   :shadow-port 7889 \
+#   :shadow-build '"server"' \
+#   :project-dir '"/Users/choomnuanb/dev/b12n-newsapi"'
+
 clojure -X:mcp-shadow-dual \
   :port 7888 \
   :shadow-port 7889 \
   :shadow-build '"server"' \
-  :project-dir '"/Users/choomnuanb/dev/github--chr15m--sitefox/examples/shadow-cljs-extended"'
+  :project-dir '"/Users/choomnuanb/dev/b12n-cljsapp"'
